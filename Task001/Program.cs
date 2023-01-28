9e164fc008d1c39f07adad4d9a009fc011d281a3
Zosyja <zosyja@bk.ru>
2023-01-28 17:14:47 +0300

    added Task001

---
+++ Task001/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+void FillMas(int[] arr)
+{
+    for (int i=0; i<arr.Length; i++)
+    {
+        int num = new Random().Next(100, 1000);
+        arr[i] = num;
+    }
+}
+void PrintMas(int[] arr)
+{
+    foreach (var el in arr)
+    {
+        Console.Write(el + " ");
+    }
+    Console.WriteLine();
+}
+int Even(int[] arr)
+{
+    int count = 0;
+    for (int j=0; j<arr.Length; j++)
+    {
+        if (arr[j]%2==0) count++;
+    }
+    return count;
+}
+
+Console.Write("Введите размерность массива: ");
+int k = int.Parse(Console.ReadLine() ?? "0");
+int[] arr = new int[k];
+FillMas(arr);
+PrintMas(arr);
+Console.Write("Количество четных чисел в массиве = ");
+Console.WriteLine(Even(arr));
